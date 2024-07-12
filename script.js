1ac65a213a3b11ce4bd32355747c6d0f86e95196
Keiralowden17 <163159887+Keiralowden17@users.noreply.github.com>
2024-07-12 11:46:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('highlightButton').addEventListener('click', function() {
+    var textToHighlight = document.getElementById('textToHighlight');
+    textToHighlight.classList.toggle('highlighted');
+});
